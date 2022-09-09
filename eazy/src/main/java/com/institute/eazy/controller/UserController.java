024689b76bffd85f7effe1715bf9224874a677c4
vjrathee <ratheevvv@gmail.com>
2022-09-09 21:25:32 +0530

    Simple commit again 33

--- eazy/src/main/java/com/institute/eazy/controller/UserController.java
+++ eazy/src/main/java/com/institute/eazy/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RequestMapping(value = "/eazy")
 // This is simple comment
 // another comment
+// third commit
 public class UserController {
     @Autowired
     UserService userService;
